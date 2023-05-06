a9179790315473e04834ce6647cc90dee0903de9
thurmanzero <thurmantsao@gmail.com>
2023-05-06 20:30:57 +0800

    imac

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue';
+import { createPinia } from "pinia"
 import App from './App.vue'
 import router from './router'
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(router).mount('#app')
+
+createApp(App)
+    .use(createPinia())
+    .use(router)
+    .use(Antd)
+    .mount('#app')
